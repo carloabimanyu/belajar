aaa63c09ef05d269d3ebb4c8a01aafe3c9324ecf
carloabimanyu <carloabimanyu11@gmail.com>
2025-11-18 16:47:55 +0700

    checkpoint 2025-11-18

---
+++ data/belajar-fundamental-deep-learning/simple_tf_keras.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+
+def main():
+    X = np.array([-4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0], dtype=float)
+    y = np.array([5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0], dtype=float)
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(units=1, input_shape=[1])
+    ])
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+    model.fit(X, y, epochs=100)
+
+    print(model.predict(np.array([4, 5])))
+
+if __name__ == "__main__":
+    main()
